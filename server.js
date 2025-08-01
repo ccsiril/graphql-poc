cce30ed14435852f0b69e4fc0d353a467444f880
Cyril <user@Cyrils-MacBook-Air.local>
2025-08-01 12:46:08 +0530

    added new user as test

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const { buildSchema } = require('graphql');
 
 // In-memory user store
 const users = [
-  { id: 1, name: 'Alice', email: 'alice@example.com' }
+  { id: 1, name: 'Alice', email: 'alice@example.com' },
+  { id: 2, name: 'lake', email: 'lake@example.com' }
 ];
 
 // Define schema
